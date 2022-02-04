54138944e4734222a87f34ab3be46dc96be7c43d
Yuriy <yfrolou@mail.ru>
2022-02-04 17:51:46 +0300

    changed .gitignore

---
+++ public/scripts/user.js
@@ -0,0 +1,16 @@
+import { renderBlock } from './lib.js';
+export function renderUserBlock(userName, avatarLink, favoriteItemsAmount) {
+    const favoritesCaption = favoriteItemsAmount ? favoriteItemsAmount : 'ничего нет';
+    const hasFavoriteItems = favoriteItemsAmount >= 1;
+    renderBlock('user-block', `
+    <div class="header-container">
+      <img class="avatar" src=${avatarLink} alt=${userName} />
+      <div class="info">
+          <p class="name">${userName}</p>
+          <p class="fav">
+            <i class="heart-icon ${hasFavoriteItems ? ' active' : ''}"></i>${favoritesCaption}
+          </p>
+      </div>
+    </div>
+    `);
+}
