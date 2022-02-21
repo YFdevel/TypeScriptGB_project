1e59903d4900e5da732e33020ebb01fbbe8f714a
Yuriy <yfrolou@mail.ru>
2022-02-21 23:58:45 +0300

    lesson5

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,9 +3,10 @@ import { renderSearchStubBlock } from './search-results.js';
 import { renderUserBlock } from './user.js';
 import { renderToast } from './lib.js';
 import { getUserData } from './getUserData.js';
-import { getFavoritesAmount } from "./getFavoritesAmount.js";
-import { getCollection, getTodosByCount } from "./getCollection.js";
-import { cloneDate, addDays, FlatRentSdk, database } from "./flat-rent-sdk.js";
+import { getFavoritesAmount } from './getFavoritesAmount.js';
+import { getCollection, getTodosByCount } from './getCollection.js';
+import { cloneDate, addDays, FlatRentSdk, database } from './flat-rent-sdk.js';
+import { MyRectangle, MyCircle } from './inheritance.js';
 getCollection("users", 1).catch((error) => {
     throw error;
 });
@@ -32,7 +33,12 @@ console.log(addDays(new Date(), 3));
 const flatRentSdk = new FlatRentSdk();
 console.log(flatRentSdk.database);
 console.log(flatRentSdk.get('ab2e2'));
-console.log(flatRentSdk.search({ checkInDate: new Date(2022, 5, 1), checkOutDate: new Date(2022, 5, 12), city: 'Санкт-Петербург', priceLimit: 4500 }));
+console.log(flatRentSdk.search({
+    checkInDate: new Date(2022, 5, 1),
+    checkOutDate: new Date(2022, 5, 12),
+    city: 'Санкт-Петербург',
+    priceLimit: 4500
+}));
 console.log(flatRentSdk.book('vnd331', new Date(2022, 5, 1), new Date(2022, 5, 12)));
 console.log(flatRentSdk._generateDateRange(new Date(2022, 5, 1), new Date(2022, 5, 12)));
 console.log(flatRentSdk._areAllDatesAvailable(database[0], [new Date(2022, 5, 1), new Date(2022, 5, 12)]));
@@ -57,5 +63,37 @@ window.addEventListener('DOMContentLoaded', () => {
         name: 'Понял', handler: () => {
             console.log('Уведомление закрыто');
         }
+    });
+    const circle = new MyCircle(10, { x: 10, y: 20 });
+    const rectangle = new MyRectangle(3, 4, 5, 8);
+    console.log('Площадь круга: ' + circle.square().toFixed(2));
+    console.log('Площадь прямоугольника: ' + rectangle.square());
+    console.log('Высота прямоугольника: ' + rectangle.height());
+    console.log('Ширина прямоугольника: ' + rectangle.width());
+    document.getElementById('horizontal').addEventListener('input', (event) => {
+        const { target } = event;
+        const moveHorizontal = target;
+        const horizontalRec = rectangle.moveHorizontal(Number(moveHorizontal.value));
+        const horizontalCircle = circle.moveHorizontal(Number(moveHorizontal.value));
+        document.getElementById('rectangle-to-move').style.left = `${horizontalRec}px`;
+        document.getElementById('circle-to-move').style.left = `${horizontalCircle}px`;
+    });
+    document.getElementById('vertical').addEventListener('input', (event) => {
+        const { target } = event;
+        const moveVertical = target;
+        const verticalRec = rectangle.moveVertical(Number(moveVertical.value));
+        const verticalCircle = circle.moveVertical(Number(moveVertical.value));
+        document.getElementById('rectangle-to-move').style.top = `${verticalRec}px`;
+        document.getElementById('circle-to-move').style.top = `${verticalCircle}px`;
+    });
+    document.getElementById('add-rectangle').addEventListener('click', (event) => {
+        const div = document.createElement('div');
+        document.body.appendChild(div);
+        div.insertAdjacentHTML("beforeend", rectangle.paintRectangle(50, 300, 150, 150));
+    });
+    document.getElementById('add-circle').addEventListener('click', (event) => {
+        const div = document.createElement('div');
+        document.body.appendChild(div);
+        div.insertAdjacentHTML("beforeend", circle.paintCircle(300, 500, 200));
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import {renderSearchStubBlock} from './search-results.js'
 import {renderUserBlock} from './user.js'
 import {renderToast} from './lib.js'
 import {getUserData} from './getUserData.js'
-import {getFavoritesAmount} from "./getFavoritesAmount.js";
-import {getCollection,getTodosByCount} from "./getCollection.js";
-import {cloneDate,addDays,FlatRentSdk,database} from "./flat-rent-sdk.js";
-
+import {getFavoritesAmount} from './getFavoritesAmount.js';
+import {getCollection, getTodosByCount} from './getCollection.js';
+import {cloneDate, addDays, FlatRentSdk, database} from './flat-rent-sdk.js';
+import {MyGraphicsPrimitive2D, MyAreaPrimitive2D, MyRectangle, MyCircle} from './inheritance.js';
 
 getCollection("users", 1).catch((error?: string) => {
   throw error
@@ -31,15 +31,20 @@ getTodosByCount(7).catch((error?: string) => {
 });
 
 console.log(cloneDate(new Date()))
-console.log(addDays(new Date(),3))
-const flatRentSdk=new FlatRentSdk()
+console.log(addDays(new Date(), 3))
+const flatRentSdk = new FlatRentSdk()
 console.log(flatRentSdk.database)
 console.log(flatRentSdk.get('ab2e2'))
-console.log(flatRentSdk.search({checkInDate:new Date(2022,5,1),checkOutDate:new Date(2022,5,12),city:'Санкт-Петербург',priceLimit:4500}))
-console.log(flatRentSdk.book('vnd331',new Date(2022,5,1),new Date(2022,5,12)))
-console.log(flatRentSdk._generateDateRange(new Date(2022,5,1),new Date(2022,5,12)))
-console.log(flatRentSdk._areAllDatesAvailable(database[0],[new Date(2022,5,1),new Date(2022,5,12)]))
-console.log(flatRentSdk. _formatFlatObject(database[1],2))
+console.log(flatRentSdk.search({
+  checkInDate: new Date(2022, 5, 1),
+  checkOutDate: new Date(2022, 5, 12),
+  city: 'Санкт-Петербург',
+  priceLimit: 4500
+}))
+console.log(flatRentSdk.book('vnd331', new Date(2022, 5, 1), new Date(2022, 5, 12)))
+console.log(flatRentSdk._generateDateRange(new Date(2022, 5, 1), new Date(2022, 5, 12)))
+console.log(flatRentSdk._areAllDatesAvailable(database[0], [new Date(2022, 5, 1), new Date(2022, 5, 12)]))
+console.log(flatRentSdk._formatFlatObject(database[1], 2))
 // flatRentSdk. _writeDatabase({
 //   id: 'vvv111',
 //   title: 'New Hotel',
@@ -65,5 +70,41 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     }
   )
+  const circle = new MyCircle(10, {x: 10, y: 20});
+  const rectangle = new MyRectangle(3, 4, 5, 8);
+  console.log('Площадь круга: ' + circle.square().toFixed(2));
+  console.log('Площадь прямоугольника: ' + rectangle.square());
+  console.log('Высота прямоугольника: ' + rectangle.height());
+  console.log('Ширина прямоугольника: ' + rectangle.width());
 
+  document.getElementById('horizontal').addEventListener('input', (event: Event) => {
+    const {target} = event;
+    const moveHorizontal = target as HTMLButtonElement;
+    const horizontalRec = rectangle.moveHorizontal(Number(moveHorizontal.value));
+    const horizontalCircle = circle.moveHorizontal(Number(moveHorizontal.value));
+
+    document.getElementById('rectangle-to-move').style.left = `${horizontalRec}px`;
+    document.getElementById('circle-to-move').style.left = `${horizontalCircle}px`;
+  })
+
+  document.getElementById('vertical').addEventListener('input', (event: Event) => {
+    const {target} = event;
+    const moveVertical = target as HTMLButtonElement;
+    const verticalRec = rectangle.moveVertical(Number(moveVertical.value));
+    const verticalCircle = circle.moveVertical(Number(moveVertical.value));
+
+    document.getElementById('rectangle-to-move').style.top = `${verticalRec}px`;
+    document.getElementById('circle-to-move').style.top = `${verticalCircle}px`;
+  })
+
+  document.getElementById('add-rectangle').addEventListener('click', (event) => {
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    div.insertAdjacentHTML("beforeend", rectangle.paintRectangle(50, 300, 150, 150));
+  })
+  document.getElementById('add-circle').addEventListener('click', (event) => {
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    div.insertAdjacentHTML("beforeend", circle.paintCircle(300, 500, 200));
+  })
 })
